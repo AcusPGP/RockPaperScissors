4c6585bb6acf45fbe3022e17cf3ba8b70836cbd6
Acus <acus1304pgphcm@gmail.com>
2022-10-04 02:03:12 +0700

    Complete the project.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.rockpaperscissors {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires javafx.media;
+
+    opens com.example.rockpaperscissors to javafx.fxml;
+    exports com.example.rockpaperscissors;
+}
